c171b5be653fc54bec0645d48c10af13bf4fe031
Thinh Nguyen <thinhmnguyen4105@gmail.com>
2025-08-21 13:39:11 -0400

    implement functions for cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
+import ProductList from './ProductList';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const { name, image, cost } = action.payload;
 
-    
+            const existedItem = state.items.find(item => item.name === name);
+            if (existedItem) {
+                existedItem.quantity++;
+            }
+
+            else {
+                state.items.push({ name, image, cost, quantity: 1 })
+            }
+
+        },
+
+        removeItem: (state, action) => {
+            state.items = state.items.filter(item => item.name !== action.payload);
+        },
+        updateQuantity: (state, action) => {
+            const { name, quantity } = action.payload;
+            const targetItem = state.items.find(item => item.name === name);
+
+            if (targetItem) {
+                targetItem.quantity = quantity;
+            }
+        },
     },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
